73763945797d13ca13fb7ee0681089390012c29b
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2024-02-27 15:22:30 +0200

    Switch LLaVA 13B to 7B

--- CKPT_PTH.py
+++ CKPT_PTH.py
@@ -1,4 +1,4 @@
 LLAVA_CLIP_PATH = 'openai/clip-vit-large-patch14-336'
-LLAVA_MODEL_PATH = 'liuhaotian/llava-v1.5-13b'
+LLAVA_MODEL_PATH = 'liuhaotian/llava-v1.5-7b'
 SDXL_CLIP1_PATH = 'openai/clip-vit-large-patch14'
 SDXL_CLIP2_CKPT_PTH = 'models/open_clip_pytorch_model.bin'
